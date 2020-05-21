bd8eee2265a54000fbdf1feff5c6e4f9de8bd922
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-05-21 23:02:52 +0530

    transaction class for adding all the details of transactions

---
+++ lib/transaction.dart
@@ -0,0 +1,16 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  //create a constructor
+
+  Transaction(
+    {
+    this.id, 
+    this.title, 
+    this.amount, 
+    this.date});
+
+}
