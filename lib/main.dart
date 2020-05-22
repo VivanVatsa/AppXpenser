74d1227aa7efed18348140334321c9c33648bc29
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-05-22 22:28:18 +0530

    added transaction functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:expense_app/transaction.dart';
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -18,7 +18,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [];
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.54,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,10 +57,58 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Card(
-            color: Colors.red,
-            child: Text('LIST FOR TX'),
-          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple, 
+                        width: 2
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+
+
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                    // Container(
+                    //   margin: EdgeInsets.symmetric(
+                    //     vertical: 15,
+                    //     horizontal: 25,
+                    //   ),
+                    // )
+                    Text(
+                      tx.title, 
+                      style: TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      tx.date.toString(),
+                      style: TextStyle(color: Colors.grey),
+                      ),
+                  ],),
+                ],),
+              );
+            }).toList(),
+          )
         ],
       ),
     );
