e897129095d4e2efc1ae1980be67ce81a93c1595
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-05-29 14:06:52 +0530

    Create transaction_list.dart

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionList extends StatefulWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: _userTransactions.map((tx) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 15,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple, width: 2),
+                ),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  '\$${tx.amount}',
+                  // + tx.amount.toString()
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  // Container(
+                  //   margin: EdgeInsets.symmetric(
+                  //     vertical: 15,
+                  //     horizontal: 25,
+                  //   ),
+                  // )
+                  Text(
+                    tx.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    // DateFormat('yyyy/MM/dd').format(tx.date),
+                    // DateFormat.yMMMd().add_jms().format(tx.date),
+                    DateFormat.yMMMd().format(tx.date),
+                    // DateFormat().format(tx.date),
+                    // tx.date.toString(),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    );
+  }
+}
