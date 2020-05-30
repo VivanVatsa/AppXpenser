75df2cea06c23d7984b2c423110bb7087d11a467
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-05-30 14:32:05 +0530

    Update user_transactions.dart

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -3,16 +3,14 @@ import './transaction_list.dart';
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 
- class UserTransactions extends StatefulWidget {
-   @override
-   
-   _UserTransactionsState createState() => _UserTransactionsState();
- }
- 
- 
- class _UserTransactionsState extends State<UserTransactions> {
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
 
-   final List<Transaction> _userTransactions = [
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    // this _usertransaction is a pointer that point into the transaction list
     Transaction(
       id: 't1',
       title: 'New Shoes',
@@ -26,12 +24,30 @@ import '../models/transaction.dart';
       date: DateTime.now(),
     ),
   ];
-   @override
-   Widget build(BuildContext context) {
-     return Column(children: <Widget>[
-       NewTransaction(),
-       TransactionList(),
+
+// use underscore to mark the this method is private or part of this particular class
+// exclusive accessible or like the scope inside this class only
+  void _addNewTransaction (String txTitle, double txAmount) {
+    
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransactions),
       ],
-     );
-   }
- }
+    );
+  }
+}
