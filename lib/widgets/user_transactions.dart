930b1de19828db987da0c4368c92befc07fa2683
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-03 15:41:19 +0530

    imporivng the overall performance and removing the bugs

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -10,7 +10,6 @@ class UserTransactions extends StatefulWidget {
 
 class _UserTransactionsState extends State<UserTransactions> {
   final List<Transaction> _userTransactions = [
-    // this _usertransaction is a pointer that point into the transaction list
     Transaction(
       id: 't1',
       title: 'New Shoes',
@@ -23,6 +22,40 @@ class _UserTransactionsState extends State<UserTransactions> {
       amount: 16.54,
       date: DateTime.now(),
     ),
+  ];
+
+  //for creating a new transaction when the button is pressed for adding new transaction
+  void _addNewTransaction(String txTitle, double txAmount){
+    final newTx = Transaction(
+      title: txTitle, 
+      amount: txAmount, 
+      date: DateTime.now(), 
+      id: DateTime.now().toString(),
+      );
+
+      setState(() {
+        _userTransactions.add(newTx);
+      });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      NewTransaction(),
+      TransactionList(_userTransactions),
+    ],);
+  }
+}
+
+/*
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    // this _usertransaction is a pointer that point into the transaction list
+    
   ];
 
 // use underscore to mark the this method is private or part of this particular class
