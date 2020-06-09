3360bcf386503474b8f86004c745d6a350a0cf4b
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-09 18:45:24 +0530

    adding add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 // import './widgets/transaction_list.dart';
-import './widgets/user_transactions.dart';
-
-
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
-// import './models/transaction.dart';
+import './models/transaction.dart';
 // import './models/transaction.dart';
 
 // void main() {
@@ -22,19 +21,71 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
 
   // String titleInput;
   // String amountInput;
 
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.54,
+      date: DateTime.now(),
+    ),
+  ];
+
+  //for creating a new transaction when the button is pressed for adding new transaction
+  void _addNewTransaction(String txTitle, double txAmount)
+  {
+    final newTx = Transaction(
+      title: txTitle, 
+      amount: txAmount, 
+      date: DateTime.now(), 
+      id: DateTime.now().toString(),
+      );
+
+      setState(() {
+        _userTransactions.add(newTx);
+      });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx){
+    showModalBottomSheet(
+      context: ctx, 
+      builder: (_){
+      return GestureDetector(
+        onTap: () {},
+        child: NewTransaction(_addNewTransaction),
+        behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          //we actually add action button in this widget prop
+          IconButton(icon: Icon(Icons.add), 
+          onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
               child: Column(
@@ -56,11 +107,18 @@ class MyHomePage extends StatelessWidget {
                   elevation: 5,
                 ),
               ),
-              UserTransactions()
+                    TransactionList(_userTransactions),
+
               // UserTransactions(),
             ],
           ),
       ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+        ),
     );
   }
 }
