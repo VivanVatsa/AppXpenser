75b1a79f23cc361149448fb6fb999f175fd0e3d4
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-18 12:53:15 +0530

    lil tweaks update

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,82 +13,52 @@ class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   TransactionList(this.transactions);
   @override
-
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: transactions.isEmpty ? Column(children: <Widget>[
-        Text('No transactions Added yet!',
-        style: Theme.of(context).textTheme.headline6,
-         ),
-         SizedBox( // used as separators between 2 components in the application
-           height: 50,
-         ),
-         Container(
-           height: 200,
-           child: Image.asset(
-             'assets/images/waiting.png',
-             fit: BoxFit.cover,)),
-      ],
-      ) :  ListView.builder(
-        // must have attributes of the this current widget and to keep in mind that this has to be applied to all the widgets
-        itemBuilder: (ctx, index){
-          return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor, 
-                        width: 2,
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No transactions Added yet!',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  // used as separators between 2 components in the application
+                  height: 50,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            )
+          : ListView.builder(
+              // must have attributes of the this current widget and to keep in mind that this has to be applied to all the widgets
+              itemBuilder: (ctx, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6),
+                      child: FittedBox(
+                        child: Text('\₹${transactions[index].amount}'),
                         ),
                     ),
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      //by adding the tostringasfixed property which rounds off the value entered in the amount
-                      '\₹${transactions[index].amount.toStringAsFixed(2)}',
-                      // + tx.amount.toString()
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Theme.of(context).primaryColor,
-                      ),
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.headline6,
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // Container(
-                      //   margin: EdgeInsets.symmetric(
-                      //     vertical: 15,
-                      //     horizontal: 25,
-                      //   ),
-                      // )
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      Text(
-                        // DateFormat('yyyy/MM/dd').format(tx.date),
-                        // DateFormat.yMMMd().add_jms().format(tx.date),
-                        DateFormat.yMMMd().format(transactions[index].date),
-                        // DateFormat().format(tx.date),
-                        // tx.date.toString(),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            );
-
-        },
-        itemCount: transactions.length,
-        ),
+                  subtitle: Text(
+                    DateFormat.yMMMd().format( transactions[index].date),
+                    ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
