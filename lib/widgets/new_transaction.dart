53a89cc06673de016e27990aa36100858c6798c8
VivanVatsa <VivanVatsa@users.noreply.github.com>
2020-06-22 13:34:01 +0530

    Update new_transaction.dart

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -15,7 +15,7 @@ class _NewTransactionState extends State<NewTransaction> {
 
   final amountController = TextEditingController();
 
-  void submitData() {
+  void _submitData() {
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
 
